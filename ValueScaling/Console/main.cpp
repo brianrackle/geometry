c8b39cf51d652f31d0dbc5981d54d0783988cd0a
brianrackle <brianrackle@hotmail.com>
2014-07-29 23:46:22 -0700

    Adding template types where they were left out. From and to can now use different types.

--- ValueScaling/Console/main.cpp
+++ ValueScaling/Console/main.cpp
@@ -1,22 +1,20 @@
 // ConsoleApplication1.cpp : Defines the entry point for the console application.
 //
 
-#include <string>
-#include <regex>
-#include <algorithm>
-#include <numeric>
-#include <fstream>
-#include <chrono>
+#include <limits>
 
-template <class T>
-T scaleRange(T lowestFrom, T highestFrom, T lowestTo, T highestTo, T value)
+
+//needs to handle different types for from and to
+template <class _fT, class _tT>
+_tT scaleRange(_fT value, _fT lowestFrom, _fT highestFrom, _tT lowestTo, _tT highestTo)
 {
 	//scale by half to account for negative and positive range being too large to represent
-	const auto HLF = [](T v){ return v / 2; }; 
+	const auto && fHLF = [](_fT v){ return v / 2; };
+	const auto && tHLF = [](_fT v){ return v / 2; };
 
-	double scaledOffsetResult = 
-		((HLF(highestTo) - HLF(lowestTo)) * ((HLF(value) - HLF(lowestFrom)) 
-		/ (HLF(highestFrom) - HLF(lowestFrom))));
+	_fT scaledOffsetResult =
+		((tHLF(highestTo) - tHLF(lowestTo)) * ((fHLF(value) - fHLF(lowestFrom))
+		/ (fHLF(highestFrom) - fHLF(lowestFrom))));
 	return scaledOffsetResult + lowestTo + scaledOffsetResult; //seperated to prevent overflow
 }
 
@@ -25,26 +23,11 @@ int main()
 	double lowestFrom = std::numeric_limits<double>::lowest();
 	double highestFrom = std::numeric_limits<double>::max();
 
-	double lowestTo = std::numeric_limits<double>::lowest();
-	double highestTo = std::numeric_limits<double>::max();
+	unsigned int lowestTo = std::numeric_limits<unsigned int>::lowest();
+	unsigned int highestTo = std::numeric_limits<unsigned int>::max();
 
-	double value = 0;
-
-	std::ofstream log("log.txt");
-	size_t wrong = 0;
-
-	std::chrono::high_resolution_clock timer;
-	auto start = timer.now();
-	for (size_t i = 0; i < 100000000; ++i)
-	{
-		auto result0 = scaleRange(lowestFrom, highestFrom, lowestTo, highestTo, value+i);
-		if (result0 != value)
-			++wrong;
-	}
-	auto elapsed = timer.now() - start;
-	
-	log << "Time: " << elapsed.count() << "\n";
-	log << "Wrong: " << wrong;
+	double value = highestFrom;
+	auto result = scaleRange(value, lowestFrom, highestFrom, lowestTo, highestTo);
 
 	return 0;
 }
